1c806ffefe56c416597a457fb46911d373e750af
Ryan Howard <rhow97@gmail.com>
2021-10-13 15:13:42 +1000

    Updated twitter.js. Stream now working.
    Added connection to filtered stream based off of node.js sample doc from official Twitter repo - https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Filtered-Stream/filtered_stream.js
    
    Will refactor into endpoint and return as JSON

--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var router = express.Router();
 const axios = require("axios");
+var needle = require('needle');
+
+// Set Authorization bearer for all axios requests
 axios.defaults.headers.common = {
-  Authorization: `Bearer ${process.env.TWITTER_BEARER}`,
+  "authorization": `Bearer ${process.env.TWITTER_BEARER}`,
 };
+
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream';
+
+const rules = [{
+  'value': 'dog has:images',
+  'tag': 'dog pictures'
+},
+];
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
@@ -13,12 +25,135 @@ router.get("/", async function (req, res, next) {
         `https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev`
       )
     ).data;
+    console.log(apiData);
     res.status(200).json(apiData);
   } catch (e) {
     console.error(e.response);
-    console.log("Bearer", process.env.TWITTER_BEARER);
     res.status(400).json(e);
   }
 });
+
+async function getAllRules() {
+  const response = await needle('get', rulesURL, {
+    headers: {
+        "authorization": `Bearer ${process.env.TWITTER_BEARER}`
+    }
+})
+
+if (response.statusCode !== 200) {
+    console.log("Error:", response.statusMessage, response.statusCode)
+    throw new Error(response.body);
+}
+
+return (response.body);
+}
+
+async function deleteAllRules(rules) {
+  if (!Array.isArray(rules.data)) {
+    return null;
+}
+
+const ids = rules.data.map(rule => rule.id);
+
+const data = {
+    "delete": {
+        "ids": ids
+    }
+}
+
+const response = await needle('post', rulesURL, data, {
+    headers: {
+        "content-type": "application/json",
+        "authorization": `Bearer ${process.env.TWITTER_BEARER}`
+    }
+})
+
+if (response.statusCode !== 200) {
+    throw new Error(response.body);
+}
+
+return (response.body);
+}
+
+async function setRules() {
+  const data = {
+    "add": rules
+  }
+
+  const response = await axios.post(rulesURL, data);
+
+  if (response.status !== 201) {
+    throw new Error(response.body);
+  }
+
+  return (response.body);
+}
+
+function streamConnect(retryAttempt) {
+
+  const stream = needle.get(streamURL, {
+    headers: {
+      "User-Agent": "v2FilterStreamJS",
+      "Authorization": `Bearer ${process.env.TWITTER_BEARER}`
+    },
+    timeout: 20000
+  });
+
+  console.log("THIS IS THE STREAM", stream)
+
+  stream.on('data', data => {
+    try {
+      const json = JSON.parse(data);
+      console.log(json);
+      // A successful connection resets retry count.
+      retryAttempt = 0;
+    } catch (e) {
+      if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+        console.log(data.detail)
+        process.exit(1)
+      } else {
+        // Keep alive signal received. Do nothing.
+      }
+    }
+  }).on('err', error => {
+    if (error.code !== 'ECONNRESET') {
+      console.log(error.code);
+      process.exit(1);
+    } else {
+      // This reconnection logic will attempt to reconnect when a disconnection is detected.
+      // To avoid rate limits, this logic implements exponential backoff, so the wait time
+      // will increase if the client cannot reconnect to the stream. 
+      setTimeout(() => {
+        console.warn("A connection error occurred. Reconnecting...")
+        streamConnect(++retryAttempt);
+      }, 2 ** retryAttempt)
+    }
+  });
+
+  return stream;
+
+}
+
+(async () => {
+  let currentRules;
+
+  try {
+    // Gets the complete list of rules currently applied to the stream
+    currentRules = await getAllRules();
+
+    // Delete all rules. Comment the line below if you want to keep your existing rules.
+    await deleteAllRules(currentRules);
+
+    // Add rules to the stream. Comment the line below if you don't want to add new rules.
+    await setRules();
+
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+
+  // Listen to the stream.
+  streamConnect(0);
+})();
 
 module.exports = router;
