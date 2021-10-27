f19fb8476f28468c5793e45e80b2fea332aa69b7
HwaiXin <meganlau1982@gmail.com>
2021-10-28 08:32:58 +1000

    Implemented persistence to Redis and S3, storing historical data

--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -2,6 +2,9 @@ var express = require("express");
 var router = express.Router();
 const axios = require("axios");
 var needle = require("needle");
+var storage = require("../components/storage");
+const redis = require('redis');
+var AWS = require('aws-sdk');
 
 var natural = require("natural");
 var Analyzer = natural.SentimentAnalyzer;
@@ -43,12 +46,13 @@ router.get("/", async function (req, res, next) {
 router.get("/:search", async function (req, res, next) {
   let { search } = req.params;
   let results = [];
-  try {
-    let apiData = (
-      await axios.get(
-        `https://api.twitter.com/2/tweets/search/recent?query=${search} lang:en&tweet.fields=created_at`
-      )
-    ).data.data;
+  const params = { Bucket: storage.bucketName, Key: search};
+
+  // Serve directly from Twitter API and store in Redis and S3
+  return axios.get(`https://api.twitter.com/2/tweets/search/recent?query=${search} lang:en&tweet.fields=created_at`)
+  .then(response => {
+    const apiData = response.data.data;
+
     apiData.forEach((tweet) => {
       let text = tweet.text; // Get text from returned tweet
       let created_at = tweet.created_at; // Get ISO creation date
@@ -63,12 +67,53 @@ router.get("/:search", async function (req, res, next) {
         });
       }
     });
-    console.log(results);
-    res.status(200).json(results);
-  } catch (e) {
-    console.error(e.response);
-    res.status(400).json(e);
-  }
+
+    //Check redis for key
+    storage.redisClient.get(search, (err, result) => {
+    //If that key exist
+    if (result) 
+    {
+      //Serve data from Redis along with current data
+      const json = JSON.parse(result);
+      const resultJSON = json.data.concat(results);
+      //Store new data
+      storage.storeRedis(search, resultJSON);
+      storage.storeS3(search, resultJSON);
+      res.status(200).json(resultJSON);
+    }
+    else 
+    { 
+      console.log("CHECK S3");
+      // Check S3 bucket
+      new AWS.S3({apiVersion: '2006-03-01'}).getObject(params, (err, result) => {
+        // If key exists
+        if (result) 
+        {
+          // Serve from S3
+          const json = JSON.parse(result.Body);
+          const resultJSON = json.data.concat(results);
+          //Store new data
+          storage.storeRedis(search, resultJSON);
+          storage.storeS3(search, resultJSON);
+          res.status(200).json(resultJSON);
+        }
+        //If key has not been stored anywhere
+        else 
+        {
+          console.log("DATA ONLY FROM API");
+          console.log(results);
+          //Store new data
+          storage.storeRedis(search, results);
+          storage.storeS3(search, results);
+          res.status(200).json(results);
+        }
+      });
+    }
+    });
+  })
+  .catch(err => {
+    return res.json(err);
+  });
 });
 
 function scoreToString(score) {
